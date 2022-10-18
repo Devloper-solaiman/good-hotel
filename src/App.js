03de344e9fe9813f1a69858afd6724c02f50f80e
devloper solaiman <you@example.com>
2022-10-18 18:55:11 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h4 className='text-primary'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis ducimus deleniti distinctio veniam cum fugiat dolorem aliquid natus! Provident facere doloremque expedita odio aperiam vitae rerum corporis, ipsum nulla in!</h4>
     </div>
   );
 }
